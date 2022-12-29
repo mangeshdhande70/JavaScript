be5315457cb6fb70cf38aeee6a66432e3e908b5a
Mangesh Dhande <102177719+mangeshdhande70@users.noreply.github.com>
2022-12-29 10:00:40 +0000

    added

--- Basics/02_DataTypes.js
+++ Basics/02_DataTypes.js
@@ -1,21 +1,48 @@
 
-const userId = 1234
+const userId =1204432 ;
 let email =  "md7@gmail.com"
 var password = "Md71998"
 variable = "Chris Woaks"
 let  name;
 
 
-// const keyword varaible userId = 123 --> U cannot modify this variable bcz it is constant
+/*
+
+          const ---> Keyword
+ ================================================
+
+* To declare constant variable and u cannot modify the values of that variable.
+
+* always need to initialize the constant variable at the time of it's Declaration otherwise 
+  Missing initializer in const declaration Error occurs.
+
+*/
+
+// userId = 123 --> U cannot modify this variable bcz it is constant
 
 
-// let keyword recommended to use 
+/*
+                     let keyword
+     =============================================== 
+      
+     * let variable is always recommended to use and this let keyword solve the 
+       problem of block Scope and function scope.
+
+     */
 email = "mangesh70@gmail.com";
 
-// var keyword not recommended to use bcz it geneerate scope problem 
+
+/* 
+             var -- Keyword
+    ===============================================
+     
+    * Prefer not to use var bcz of issuein block scope and function scope hence, let come into picture
+*/
 password = "12345";
 
-// execute but not recommended to use
+/* 
+execute but not recommended to use
+*/
 variable = 123
 
 console.log(userId);
