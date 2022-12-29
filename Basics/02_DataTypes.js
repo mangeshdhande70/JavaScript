4654e5471e00f2e26d5c0ba8bc68abdd984e1f42
Mangesh Dhande <102177719+mangeshdhande70@users.noreply.github.com>
2022-12-29 09:52:00 +0000

    added

---
+++ Basics/02_DataTypes.js
@@ -0,0 +1,23 @@
+
+const userId = 1234
+let email =  "md7@gmail.com"
+var password = "Md71998"
+variable = "Chris Woaks"
+let  name;
+
+
+// const keyword varaible userId = 123 --> U cannot modify this variable bcz it is constant
+
+
+// let keyword recommended to use 
+email = "mangesh70@gmail.com";
+
+// var keyword not recommended to use bcz it geneerate scope problem 
+password = "12345";
+
+// execute but not recommended to use
+variable = 123
+
+console.log(userId);
+
+console.table([userId , email , password , variable , name]);
