4aeee869011e4bfdd8668a3003987455a72c14d6
Mangesh Dhande <mangesh.dhande@infosys.com>
2023-10-01 20:26:28 +0530

    Added

---
+++ 01_Basics/03_conversionAndOperations.js
@@ -0,0 +1,44 @@
+let score = "33"
+let num = "22a"
+
+console.log(typeof score);
+console.log(typeof(num));
+
+let scoreInNumber = Number(score)
+let numInNumber = Number(num)
+
+console.log(typeof scoreInNumber);
+console.log(typeof numInNumber);
+console.log(scoreInNumber);
+console.log(numInNumber)
+
+/*
+ Coverting to Number
+ ===================================================================
+
+  String consist only Number then               => "33" = 33
+  String consist alphanumeric Value then "22ab" => NaN (Not a Number)
+  Boolean convert to Number                     => true = 1, false = 0
+
+*/
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+isLoggedIn = 0
+booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let sum = 33
+let stringSum = String(sum)
+console.log(stringSum);
+console.log(typeof stringSum)
+
+
+
+// ********************* Operations *********************************
+
+
